d5723c97d97c03f602f2e3c67ee7893155f425f0
Nathan Watts <nathan@naykel.com.au>
2024-01-31 08:54:38 +1000

    added reset filepond

--- resources/views/components/v2/input/filepond.blade.php
+++ resources/views/components/v2/input/filepond.blade.php
@@ -1,9 +1,38 @@
 @props(['for' => null, 'label'])
 
-    <div x-data wire:ignore
-        x-init="
-            FilePond.registerPlugin(FilePondPluginFileValidateSize);
+<div x-data="{ initFilePond }" wire:ignore x-init="initFilePond()" class="frm-row">
+    @isset($label)
+        <x-gt-label :tooltip="$tooltip ?? null" />
+    @endisset
 
+    <input type="file" x-ref="input" class="my" style="display:none">
+
+    @error($for)
+        <small class="txt-red" role="alert"> {{ $message }} </small>
+    @enderror
+</div>
+
+@push('styles')
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
+    <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
+    <style>
+        .filepond--root {
+            margin-bottom: 0;
+        }
+        .filepond--credits {
+            display: none;
+        }
+    </style>
+@endpush
+
+@pushOnce('scripts')
+    <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
+    <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
+
+    <script>
+        function initFilePond() {
+            FilePond.registerPlugin(FilePondPluginFileValidateSize);
+            // Set global options for all FilePond instances
             FilePond.setOptions({
                 allowMultiple: {{ isset($attributes['multiple']) ? 'true' : 'false' }},
                 maxFileSize: '3MB',
@@ -16,36 +45,12 @@
                     },
                 }
             });
-            FilePond.create($refs.input);
-        "
-    class="frm-row">
-
-        @isset($label)
-            <x-gt-label :tooltip="$tooltip ?? null" />
-        @endisset
-
-        <input type="file" x-ref="input" class="my" style="display:none">
-
-        @error($for)
-            <small class="txt-red" role="alert"> {{ $message }} </small>
-        @enderror
-    </div>
-
-
-    @push('styles')
-        <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
-        <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
-        <style>
-            .filepond--root {
-                margin-bottom: 0;
-            }
-            .filepond--credits {
-                display: none;
-            }
-        </style>
-    @endpush
-
-    @pushOnce('scripts')
-        <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
-        <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
-    @endPushOnce
+            // Create a new FilePond instance and attach it to the input element
+            const pond = FilePond.create(this.$refs.input);
+            // Listen for pondReset event and remove all files
+            addEventListener('pondReset', e => {
+                pond.removeFiles();
+            });
+        }
+    </script>
+@endPushOnce
