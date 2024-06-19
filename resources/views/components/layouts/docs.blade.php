c1fd12d55ad2dc9afcb409b7553c4c77949ee682
Nathan Watts <nathan@naykel.com.au>
2024-06-20 06:00:15 +1000

    template updates

--- resources/views/components/layouts/docs.blade.php
+++ resources/views/components/layouts/docs.blade.php
@@ -2,6 +2,14 @@
 
 <x-gt-app-layout layout="base" :$pageTitle>
 
+    @if (class_exists(\Naykel\Devit\DevitServiceProvider::class))
+        @includeIf('devit::components.dev-toolbar')
+    @else
+        @if (config('authit.allow_register') && Route::has('login'))
+            @includeFirst([ 'components.layouts.partials.top-toolbar', 'gotime::components.layouts.partials.top-toolbar', ])
+        @endif
+    @endif
+
     @includeFirst(['components.layouts.partials.navbar', 'gotime::components.layouts.partials.navbar'])
 
     @isset($top)
--- resources/views/components/layouts/partials/navbar.blade.php
+++ resources/views/components/layouts/partials/navbar.blade.php
@@ -3,22 +3,16 @@ DO NOT remove the container from this layout. If you don't want
 the container then publish the layout locally and override. --}}
 
 <div class="navbar">
-
     <div class="container">
-
         <div class="logo">
             <a href="{{ url('/') }}"><img src="{{ config('naykel.logo.path') }}" alt="{{ config('app.name') }}"
                     height="{{ config('naykel.logo.height') }}" width="{{ config('naykel.logo.width') }}"></a>
         </div>
-
         <div class="flex va-c to-md:hide">
             <x-gt-menu layout="hover" class="gap-1" itemClass="nav-item rounded-05" />
         </div>
-
     </div>
-
     <div class="md:hide mxy-0">
         <x-gt-sidebar layout="burger-button-main" />
     </div>
-
 </div>
