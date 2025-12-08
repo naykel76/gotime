032eca8c08378732a522f37d1d64830aff48df17
Nathan Watts <nathan@naykel.com.au>
2025-12-09 09:27:48 +1000

    Fix CSS class naming for addons: update 'withAddons' to 'with-addons' and 'leadingAddon'/'trailingAddon' to 'leading-addon'/'trailing-addon' for consistency.

--- resources/views/components/input/partials/control-with-addons.blade.php
+++ resources/views/components/input/partials/control-with-addons.blade.php
@@ -1,13 +1,13 @@
 @aware(['leadingAddon', 'trailingAddon'])
 
 @if ($leadingAddon || $trailingAddon)
-    <div class="withAddons">
+    <div class="with-addons">
         @if ($leadingAddon)
-            <div {{ $leadingAddon->attributes->class(['leadingAddon']) }}>{{ $leadingAddon }}</div>
+            <div {{ $leadingAddon->attributes->class(['leading-addon']) }}>{{ $leadingAddon }}</div>
         @endif
         {{-- trailing addon must come before slot for JTB to manage borders correctly --}}
         @if ($trailingAddon)
-            <div {{ $trailingAddon->attributes->class(['trailingAddon']) }}>{{ $trailingAddon }}</div>
+            <div {{ $trailingAddon->attributes->class(['trailing-addon']) }}>{{ $trailingAddon }}</div>
         @endif
         {{ $slot }}
     </div>
