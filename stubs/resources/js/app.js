42f06a3c134184a379d98ed83f69051a9c6d112a
Nathan Watts <nathan@naykel.com.au>
2026-04-22 06:21:54 +1000

    Remove bootstrap.js file and clean up app.js imports

--- stubs/resources/js/app.js
+++ stubs/resources/js/app.js
@@ -1,9 +1,7 @@
-import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import collapse from '@alpinejs/collapse';
 import sort from '@alpinejs/sort'
 import './filepond';
-
 
 Alpine.plugin(sort)
 Alpine.plugin(collapse);
